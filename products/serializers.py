ecf58d8461950cc413869a14e6c62a75e762626e
Sinan <monusinan4567@gmail.com>
2026-03-11 19:27:47 +0530

    image field model update

--- products/serializers.py
+++ products/serializers.py
@@ -2,20 +2,12 @@ from rest_framework import serializers
 from .models import Products, Cart, Wishlist
 
 class ProductsSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     
     class Meta:
         model = Products
         fields = '__all__' 
 
-    def get_image(self, obj):
-      if obj.image:
-        request = self.context.get('request')
-        if request:
-            return request.build_absolute_uri(obj.image.url)
-        
-        return obj.image.url 
-      return None
+    
 
 
 class CartSerializer(serializers.ModelSerializer):
