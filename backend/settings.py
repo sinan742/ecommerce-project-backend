8b38252d25efb646605b88a110f0e66db750aaec
Sinan <monusinan4567@gmail.com>
2026-03-12 19:40:45 +0530

    add email ssl

--- backend/settings.py
+++ backend/settings.py
@@ -105,7 +105,8 @@ SIMPLE_JWT = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = 'monusinan4567@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'hdhl torb uqij lznq')
 
