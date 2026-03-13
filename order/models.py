1a0be265171b067d61b5fdb6218c93b4fba70517
Sinan <monusinan4567@gmail.com>
2026-03-13 16:28:24 +0530

    update place order foriengn key

--- order/models.py
+++ order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import uuid # 🛑 Unique ID generate cheyyaan
+from products.models import Products
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
