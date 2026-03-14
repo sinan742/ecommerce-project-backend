107fa644e9283d7b63af58e45cb093db20c825cb
Sinan <monusinan4567@gmail.com>
2026-03-14 12:24:50 +0530

    productadmin put added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import DashboarView,UserListUpdateView,ProductAdminAPIView,ProductDetailAPIView,AdminOrdersView
+from .views import DashboarView,UserListUpdateView,ProductAdminAPIView,AdminOrdersView
 
 urlpatterns =[
   path('admin-dashboard/',DashboarView.as_view()),
   path('users/', UserListUpdateView.as_view(), name='user-list'),
   path('users/<int:pk>/', UserListUpdateView.as_view(), name='user-update'),
   path('admin-products/',ProductAdminAPIView.as_view()),
-  path('admin-products/<int:pk>/',ProductDetailAPIView.as_view()),
+  path('admin-products/<int:pk>/',ProductAdminAPIView.as_view()),
   path('admin-orders/',AdminOrdersView.as_view()),
   path('admin-orders/<int:pk>/',AdminOrdersView.as_view()),
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -113,26 +113,26 @@ class ProductAdminAPIView(APIView):
 
 #---PRODUCTS PUT AND DELETE---
 
-class ProductDetailAPIView(APIView):
+# class ProductDetailAPIView(APIView):
 
-    permission_classes =[IsAdminUser]
+#     permission_classes =[IsAdminUser]
    
-    parser_classes=(MultiPartParser,FormParser)
+#     parser_classes=(MultiPartParser,FormParser)
 
-    def put(self, request, pk):
-        product = get_object_or_404(Products, pk=pk)
-        serializer = ProductAdminSerializer(product, data=request.data, partial=True)
+#     def put(self, request, pk):
+#         product = get_object_or_404(Products, pk=pk)
+#         serializer = ProductAdminSerializer(product, data=request.data, partial=True)
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
         
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Products, pk=pk)
-        product.delete()
-        return Response({"message": "Product deleted"}, status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         product = get_object_or_404(Products, pk=pk)
+#         product.delete()
+#         return Response({"message": "Product deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 # ----Admin Orders---
 
