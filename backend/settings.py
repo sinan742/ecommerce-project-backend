3ba826bb9f87d7584afe15643458ae3b832a507f
Sinan <monusinan4567@gmail.com>
2026-03-18 16:16:49 +0530

    secret evironment added

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&qz)q=)_hz3x#0*3#-3_6j4+=qpz330p@el0&df3#4fx7a1i3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
